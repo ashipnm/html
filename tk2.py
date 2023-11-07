8639e3583d5b8273cde25ae1dbfb653653422f2d
ashipnm <muhammadashique352@gmail.com>
2023-11-07 08:54:18 +0530

    f

---
+++ tk2.py
@@ -0,0 +1,6 @@
+import tkinter
+root=tkinter.Tk()
+l1=tkinter.Label(root,text="Red Sun",bg="red",fg="white").pack()
+l1=tkinter.Label(root,text="Green Grass",bg="green",fg="black").pack()
+l1=tkinter.Label(root,text="Blue Sky",bg="blue",fg="white").pack()
+root.mainloop()
